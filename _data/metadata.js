dd12dd47f21c79f7a124fe25f84ccf5c658751fc
Jason <148020318+fromjasonstuff@users.noreply.github.com>
2023-10-29 12:26:48 -0400

    Add files via upload

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "From Jason",
+	url: "https://fromjason.xyz",
+	language: "en",
+	description: "A digital garden for a physical meat bag.",
+	author: {
+		name: "Jason Velazquez",
+		email: "inbox@jvelazquez.email",
+		url: "https://fromjason.xyz/about-me/"
+	}
+}
