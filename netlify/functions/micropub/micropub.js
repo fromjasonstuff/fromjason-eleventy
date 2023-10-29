039d9f90500e15c731d14a17c3754c9c5531c366
Jason <148020318+fromjasonstuff@users.noreply.github.com>
2023-10-29 19:14:00 -0400

    Add files via upload

--- netlify/functions/micropub/micropub.js
+++ netlify/functions/micropub/micropub.js
@@ -31,17 +31,34 @@ exports.handler = (event, context, callback) => {
   // Parse the JSON event body from iA Writer
   const data = JSON.parse(event.body)
   console.log("Data: ", data);
-  
-const title = data["properties"]["name"][0]
-const content = data["properties"]["content"][0]
 
-const filename = [
+  // The format is a bit weird,
+  // where title and content are array values with a single entry
+  const title = data["properties"]["name"][0]
+  const content = data["properties"]["content"][0]
+
+  // I want the format of the filename to be yyyy-mm-dd-title-as-slug.html.md
+  // Javascript date handling is poor, (no strftime),
+  // lets hack something with the default date functions
+  // This saves us a library to import, also use a poor-mans slug generator
+  const date = new Date()
+  const filename = [
+    date.toISOString().split('T')[0], // the date
     title.replace(/[W]+/g, "-") // the slug
-  ]
+  ].join("-")
+  var fileContent = []
 
- var fileContent = []
- 
-
+  // If we've written a post without fontmatter, insert default fontmatter
+  // this allows us to override the fontmatter in iA Writer if we want, but
+  // we can also just throw out a quick article without worrying about this.
+  if (!content.includes("---")) {
+    fileContent.push("---")
+    fileContent.push('date: ' + date.toISOString())
+    fileContent.push('title: ' + title)
+    fileContent.push('sub: notebook')
+    fileContent.push('type: note')
+    fileContent.push('---')
+  }
   fileContent.push(content)
 
   // Create a new file on GitHub with the octokit library
@@ -51,7 +68,7 @@ const filename = [
     owner: "fromjasonstuff",
     repo: "fromjason-eleventy",
     message: ("Adding note: " + title),
-    path: "content/blog/" + filename + ".md",
+    path: "content/blog/" + filename + ".html.md",
     content: Buffer.from(fileContent.join("\n")).toString("base64")
   }).then((response) => {
     // Redirect iA Writer to the notes page, where the post will show up.
@@ -70,4 +87,3 @@ const filename = [
     })
   })
 }
-
