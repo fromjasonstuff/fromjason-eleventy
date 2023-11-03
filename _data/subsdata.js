16f9a6ad7067e3cc6773cfca8bf633cf44e55925
Jason <148020318+fromjasonstuff@users.noreply.github.com>
2023-11-02 21:07:44 -0400

    Add files via upload

---
+++ _data/subsdata.js
@@ -0,0 +1,39 @@
+module.exports = {
+    notebook: {
+        title: "Notebook",
+        url: "https://fromjason.xyz/notebook/",
+        language: "en",
+        description: "Very serious stuff. Typos included.",
+        author: {
+            name: "Jason Velazquez",
+            email: "inbox@jvelazquez.email",
+            url: "https://fromjason.xyz/about-me/"
+        }
+
+    },
+    freelance: {
+        title: "Freelance",
+        url: "https://fromjason.xyz/freelance/",
+        language: "en",
+        description: "Freelance? In this economy?",
+        author: {
+            name: "Jason Velazquez",
+            email: "inbox@jvelazquez.email",
+            url: "https://fromjason.xyz/about-me/"
+        }
+    },
+    stories: {
+        title: "Stories",
+        url: "https://fromjason.xyz/stories/",
+        language: "en",
+        description: "Flash fiction, short stories, & a secret third thing.",
+        author: {
+            name: "Jason Velazquez",
+            email: "inbox@jvelazquez.email",
+            url: "https://fromjason.xyz/about-me/"
+        }
+    }
+}
+
+// I can't do this until I restructure the front matter. And even then im not sure it'll work.
+// wait. i think i can.
