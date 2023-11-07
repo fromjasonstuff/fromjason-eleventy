7a2cb5562e78c8212118e1c7561376b13b21d87f
Jason <148020318+fromjasonstuff@users.noreply.github.com>
2023-11-07 14:11:52 -0500

    Add files via upload

--- content/posts/posts.11tydata.js
+++ content/posts/posts.11tydata.js
@@ -5,6 +5,6 @@ module.exports = {
 	"layout": "layouts/post.njk",
 	"permalink": "/p/{{ sub | slugify }}/{{ title | slugify }}/",
 	eleventyComputed: {
-		rssDir: data => '/p/{{ plot }}'
+		rssDir: data => '/p/{{ sub }}'
 	}
 };
