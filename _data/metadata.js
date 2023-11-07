0054b7f0e0e1dfa090df8ea0ff285c529621d8e5
Jason <148020318+fromjasonstuff@users.noreply.github.com>
2023-11-07 13:58:58 -0500

    Add files via upload

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,6 +3,7 @@ module.exports = {
 	url: "https://fromjason.xyz",
 	language: "en",
 	description: "A digital garden for a physical meat bag.",
+	openGraph: "https://fromjason.xyz/img/fromjason-opengraph.png",
 	author: {
 		name: "Jason Velazquez",
 		email: "inbox@jvelazquez.email",
