b4d694098ec0246cc4c25e97418fd64f5ae826d9
Jason <148020318+fromjasonstuff@users.noreply.github.com>
2023-12-17 20:12:32 -0500

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -14,22 +14,6 @@ const lodash = require("lodash");
 
 module.exports = function(eleventyConfig) {
 
- // set markdown footnote processor
-  let markdownIt = require("markdown-it");
-  let markdownItFootnote = require("markdown-it-footnote");
-  
-  let options = {
-    html: true, // Enable HTML tags in source
-    breaks: true,  // Convert '\n' in paragraphs into <br>
-    linkify: true // Autoconvert URL-like text to links
-  };
-  
-  // configure the library with options
-  let markdownLib =  markdownIt(options).use(markdownItFootnote);
-  // set the library to process markdown files
-  config.setLibrary("md", markdownLib);
-};
-
 eleventyConfig.addFilter("include", (arr, path, value) => {
 
  
